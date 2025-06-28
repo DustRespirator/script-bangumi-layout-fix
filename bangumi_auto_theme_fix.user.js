061438eca9012f2c505ea160b1c50087d77aa296
Q. <32119993+DustRespirator@users.noreply.github.com>
2025-06-28 14:08:13 -0500

    Create bangumi_auto_theme_fix.user.js

---
+++ bangumi_auto_theme_fix.user.js
@@ -0,0 +1,63 @@
+// ==UserScript==
+// @name         bgm.tv 浅色深色模式相关修复
+// @namespace    https://github.com/DustRespirator
+// @version      0.1
+// @description  处理用户手动使用“关灯/开灯”选项后无法响应浏览器自动切换深色模式的情况
+// @author       Hoi
+// @match        https://bgm.tv/*
+// @match        https://bangumi.tv/*
+// @match        https://chii.in/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    "use strict";
+
+    // Fix misplaced "|"
+    const listItem = document.querySelector(".content > ul > li.last");
+    if (listItem) {
+        const replaceToggleThemeText = listItem.querySelector("#toggleTheme");
+        replaceToggleThemeText.textContent = "关灯";
+    }
+
+    // Add checkbox for auto theme fix
+    const checkboxLabel = document.createElement("label");
+    checkboxLabel.style.cursor = "pointer";
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.id = "autoToggleThemeCheckbox";
+    checkbox.checked = !document.cookie.includes("chii_theme_choose=1");
+
+    // Insert checkbox
+    checkboxLabel.appendChild(checkbox);
+    checkboxLabel.append(" 自动深色模式");
+
+    listItem.append(" | ", checkboxLabel, " |");
+
+    // Remove cookie "chii_theme_choose", enable auto theme
+    function autoToggleTheme() {
+        document.cookie = "chii_theme_choose=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+        chiiLib.ukagaka.autoTheme();
+    }
+
+    // restore cookie "chii_theme_choose", refer to function updateTheme(style, remember) {}
+    function manualToggleTheme() {
+        const expires = new Date(Date.now() + 2592000 * 24 * 60 * 60 * 1000).toUTCString();
+        document.cookie = "chii_theme_choose=1; expires=" + expires + "; path=/;";
+    }
+
+    document.querySelector("#autoToggleThemeCheckbox").addEventListener("change", (e) => {
+        if (e.target.checked) {
+            autoToggleTheme();
+        } else {
+            manualToggleTheme();
+        }
+    })
+
+    document.querySelector("#toggleTheme").addEventListener("click", (e) => {
+        const checkbox = document.querySelector("#autoToggleThemeCheckbox");
+        if (checkbox) {
+            checkbox.checked = false;
+        }
+    })
+})();
