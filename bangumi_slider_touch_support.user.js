e975bc71af7d5d78aa61c9753262a0f6dbcfa52e
Q. <32119993+DustRespirator@users.noreply.github.com>
2025-09-01 06:24:42 -0500

    0.2 Add timeout counter

--- bangumi_slider_touch_support.user.js
+++ bangumi_slider_touch_support.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         bgm.tv 讨论页滑块触摸支持
 // @namespace    https://github.com/DustRespirator
-// @version      0.1
+// @version      0.2
 // @description  通过导入 jqueryui-touch-punch 解决讨论页内滑块不支持触摸操作的问题
 // @author       Hoi
 // @match        https://bgm.tv/*
@@ -11,28 +11,37 @@
 // ==/UserScript==
 
 (function() {
-    "use strict";
-
+    const sliderBar = document.getElementById("sliderContainer");
+    if (!sliderBar) {
+        return;
+    }
     const script = document.createElement("script");
-    script.src = ""; // jqueryui-touch-punch
-    document.head.appendChild(script);
+        script.src = "https://cdnjs.cloudflare.com/ajax/libs/jqueryui-touch-punch/0.2.3/jquery.ui.touch-punch.min.js";
+        document.head.appendChild(script);
 
+    let count = 0;
     script.onload = () => {
         console.log("jqueryui-touch-punch loaded");
 
         const checkInterval = setInterval(() => {
-            const $slider = $('#slider');
+            const $slider = $("#slider");
 
-            if ($slider.length && $slider.hasClass('ui-slider')) {
+            if ($slider.length && $slider.hasClass("ui-slider")) {
                 clearInterval(checkInterval);
                 console.log("slider detected");
 
-                const sliderOptions = $slider.slider('option');
-
-                $slider.slider('destroy');
+                const sliderOptions = $slider.slider("option");
+                $slider.slider("destroy");
+                console.log("slider destroyed");
 
                 $slider.slider(sliderOptions);
                 console.log("slider re-initialized");
+            }
+
+            count++;
+            if (count >= 10) {
+                clearInterval(checkInterval);
+                console.log("Reached maximum number of attempts");
             }
         }, 300);
     };
