8df7838427a1ef51bc505ecdf70e45a51d8036fb
Q. <32119993+DustRespirator@users.noreply.github.com>
2025-09-14 02:52:35 -0500

    0.4 Compatible with latest customize panel

--- bangumi_ui_optimization_mix.user.js
+++ bangumi_ui_optimization_mix.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         bgm.tv 自用 UI 优化聚合
 // @namespace    https://github.com/DustRespirator
-// @version      0.3
+// @version      0.4
 // @description  参见注释
 // @author       Hoi
 // @match        https://bgm.tv/*
@@ -13,12 +13,111 @@
 (function() {
     "use strict";
 
+    // Add Panel Tab
+    chiiLib.ukagaka.addPanelTab({
+        tab: "app_4444",
+        label: "各种 UI 调整",
+        type: "options",
+        config: [
+            {
+                title: "滑动条支持触摸操作（刷新后生效）",
+                name: "enableTouchSlider",
+                type: "radio",
+                defaultValue: "off",
+                getCurrentValue: function() {
+                    return document.cookie.includes("enable_touch_slider=1") ? "on" : "off";
+                },
+                onChange: function(value) {
+                    if (value === "on") {
+                        customizeConfigOn("enable_touch_slider");
+                    } else {
+                        customizeConfigOff("enable_touch_slider");
+                    }
+                },
+                options: [
+                    {
+                        value: "off",
+                        label: "关闭"
+                    },
+                    {
+                        value: "on",
+                        label: "开启"
+                    }
+                ]
+            },
+            {
+                title: "菜单搜索栏自动弹出软键盘（仅对移动浏览器有效）",
+                name: "enablePopupKeyboard",
+                type: "radio",
+                defaultValue: "off",
+                getCurrentValue: function() {
+                    return document.cookie.includes("enable_popup_keyboard=1") ? "on" : "off";
+                },
+                onChange: function(value) {
+                    if (value === "on") {
+                        customizeConfigOn("enable_popup_keyboard");
+                    } else {
+                        customizeConfigOff("enable_popup_keyboard");
+                    }
+                },
+                options: [
+                    {
+                        value: "off",
+                        label: "关闭"
+                    },
+                    {
+                        value: "on",
+                        label: "开启"
+                    }
+                ]
+            },
+            {
+                title: "再次点击个性化图标可关闭该面板",
+                name: "enableToggleCP",
+                type: "radio",
+                defaultValue: "off",
+                getCurrentValue: function() {
+                    return document.cookie.includes("enable_toggle_cp=1") ? "on" : "off";
+                },
+                onChange: function(value) {
+                    if (value === "on") {
+                        customizeConfigOn("enable_toggle_cp");
+                    } else {
+                        customizeConfigOff("enable_toggle_cp");
+                    }
+                },
+                options: [
+                    {
+                        value: "off",
+                        label: "关闭"
+                    },
+                    {
+                        value: "on",
+                        label: "开启"
+                    }
+                ]
+            }
+        ]
+    });
+
+    function customizeConfigOn(name) {
+        const expires = new Date(Date.now() + 365 * 24 * 60 * 60 * 1000).toUTCString();
+        document.cookie = name + "=1; expires=" + expires + "; path=/;";
+        document.documentElement.setAttribute(name, "1");
+    };
+
+    function customizeConfigOff(name) {
+        document.cookie = name + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+        document.documentElement.removeAttribute(name);
+    };
+
     //========================================================
     // Slide bar touch fix
     //========================================================
     (function() {
-        const sliderBar = document.getElementById("sliderContainer");
-        if (!sliderBar) {
+        const isEnabled = document.cookie.includes("enable_touch_slider=1");
+        const hasSliderBar = document.getElementById("sliderContainer");
+        if (!isEnabled || !hasSliderBar) {
             return;
         }
 
@@ -29,27 +128,25 @@
 
         let count = 0;
         script.onload = () => {
-            console.log("jqueryui-touch-punch loaded");
+            //console.log("jqueryui-touch-punch loaded");
 
             const checkInterval = setInterval(() => {
                 const $slider = $("#slider");
 
                 if ($slider.length && $slider.hasClass("ui-slider")) {
                     clearInterval(checkInterval);
-                    console.log("slider detected");
-
+                    //console.log("slider detected");
                     const sliderOptions = $slider.slider("option");
                     $slider.slider("destroy");
-                    console.log("slider destroyed");
-
+                    //console.log("slider destroyed");
                     $slider.slider(sliderOptions);
-                    console.log("slider re-initialized");
+                    //console.log("slider re-initialized");
                 }
 
                 count++;
                 if (count >= 10) {
                     clearInterval(checkInterval);
-                    console.log("Reached maximum number of attempts");
+                    //console.log("Reached maximum number of attempts");
                 }
             }, 300);
         };
@@ -64,73 +161,35 @@
             return;
         }
 
-        // Build setting panel for toggling keyboard
-        const autoToggleKeyboardSetting = {
-            title: "自动弹出软键盘",
-            name: "toggleKeyboard",
-            type: "radio",
-            defaultValue: "default",
-            getCurrentValue: function() {
-                return document.cookie.includes("auto_toggle_keyboard=1") ? "on" : "default";
-            },
-            onChange: function(value) {
-                if (value === "on") {
-                    autoToggleKeyboard();
-                } else {
-                    manualToggleKeyboard();
-                }
-            },
-            options: [
-                {
-                    value: "default",
-                    label: "默认（关闭）"
-                },
-                {
-                    value: "on",
-                    label: "开启"
-                }
-            ]
-        };
-        
-        // Insert toggle setting into original panel config and initialize.
-        const _originalInitCP = chiiLib.ukagaka.initCustomizePanel.bind(chiiLib.ukagaka);
-        chiiLib.ukagaka.initCustomizePanel = function() {
-            const originalGeneratePanelHTML = this.generatePanelHTML;
-            this.generatePanelHTML = function(config) {
-                config.push(autoToggleKeyboardSetting);
-                return originalGeneratePanelHTML.call(this, config);
-            }
-            _originalInitCP();
-            this.generatePanelHTML = originalGeneratePanelHTML;
-        }
-
-        // Create cookie "auto_toggle_keyboard", enable auto toggle keyboard
-        function autoToggleKeyboard() {
-            const expires = new Date(Date.now() + 2592000 * 24 * 60 * 60 * 1000).toUTCString();
-            document.cookie = "auto_toggle_keyboard=1; expires=" + expires + "; path=/;";
-        }
-
-        // Remove cookie "auto_toggle_keyboard"
-        function manualToggleKeyboard() {
-            document.cookie = "auto_toggle_keyboard=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-        }
-
+        // Focus on search box in the menu
         const searchInput = document.getElementById("search_text");
-        const checkbox = document.getElementById("navMenuNeueToggle");
+        const checkbox1 = document.getElementById("navMenuNeueToggle");
         document.querySelector("label[for='navMenuNeueToggle']").addEventListener("click", () => {
-            if (document.cookie.includes("auto_toggle_keyboard=1") && !checkbox.checked) {
+            if (document.documentElement.getAttribute("enable_popup_keyboard") === "1" && !checkbox1.checked) {
                 setTimeout(() => {
                     searchInput.focus();
-                }, 50);
+                }, 300);
             }
         });
+
+        // Focus on search box from compact button, fix incorrect focus action
+        const checkbox2 = document.getElementById("search-bar-toggle");
+        document.querySelector("label[for='search-bar-toggle']").onclick = function() {
+            document.getElementById('navMenuNeueToggle').checked = false;
+            if (searchInput && !checkbox2.checked) {
+                setTimeout(() => {
+                    searchInput.focus();
+                }, 300);
+            }
+        }
     })();
 
     //========================================================
     // Make customize icon toggle panel display/hidden
     //========================================================
     (function() {
-        if (!document.querySelector(".content")) {
+        const hasCustomizeToggle = document.querySelector(".toggle-customize");
+        if (!hasCustomizeToggle) {
             return;
         }
 
@@ -143,7 +202,7 @@
             const customizePanel = document.getElementById("customize-panel");
             const isHidden = (window.getComputedStyle(customizePanel).display === "none");
             //console.log("isHidden: ", isHidden);
-            if (customizePanel && !isHidden) {
+            if (customizePanel && !isHidden && document.documentElement.getAttribute("enable_toggle_cp") === "1") {
                 customizePanel.style.display = "none";
             } else {
                 _originalShowCP();
