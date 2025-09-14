a2ab97e2472754a9ea592df49be2bc46544a5f47
Q. <32119993+DustRespirator@users.noreply.github.com>
2025-09-14 04:21:54 -0500

    0.4.1 Fix initialize issue

--- bangumi_ui_optimization_mix.user.js
+++ bangumi_ui_optimization_mix.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         bgm.tv 自用 UI 优化聚合
 // @namespace    https://github.com/DustRespirator
-// @version      0.4
+// @version      0.4.1
 // @description  参见注释
 // @author       Hoi
 // @match        https://bgm.tv/*
@@ -12,6 +12,17 @@
 
 (function() {
     "use strict";
+
+    // Cookie name of features
+    const features = ["enable_touch_slider", "enable_popup_keyboard", "enable_toggle_cp"];
+    // Initialize html attributes
+    const html = document.documentElement;
+    features.forEach((name) => {
+        const value = getValueFromCookie(name);
+        if (value === "on") {
+            html.setAttribute(name, "on");
+        }
+    });
 
     // Add Panel Tab
     chiiLib.ukagaka.addPanelTab({
@@ -25,7 +36,7 @@
                 type: "radio",
                 defaultValue: "off",
                 getCurrentValue: function() {
-                    return document.cookie.includes("enable_touch_slider=1") ? "on" : "off";
+                    return getValueFromCookie("enable_touch_slider");
                 },
                 onChange: function(value) {
                     if (value === "on") {
@@ -51,7 +62,7 @@
                 type: "radio",
                 defaultValue: "off",
                 getCurrentValue: function() {
-                    return document.cookie.includes("enable_popup_keyboard=1") ? "on" : "off";
+                    return getValueFromCookie("enable_popup_keyboard");
                 },
                 onChange: function(value) {
                     if (value === "on") {
@@ -77,7 +88,7 @@
                 type: "radio",
                 defaultValue: "off",
                 getCurrentValue: function() {
-                    return document.cookie.includes("enable_toggle_cp=1") ? "on" : "off";
+                    return getValueFromCookie("enable_toggle_cp");
                 },
                 onChange: function(value) {
                     if (value === "on") {
@@ -100,16 +111,22 @@
         ]
     });
 
+    // Get current value from cookie
+    function getValueFromCookie(name) {
+        const pair = name + "=on";
+        return document.cookie.includes(pair) ? "on" : "off";
+    }
+
     function customizeConfigOn(name) {
         const expires = new Date(Date.now() + 365 * 24 * 60 * 60 * 1000).toUTCString();
-        document.cookie = name + "=1; expires=" + expires + "; path=/;";
-        document.documentElement.setAttribute(name, "1");
-    };
+        document.cookie = name + "=on; expires=" + expires + "; path=/;";
+        document.documentElement.setAttribute(name, "on");
+    }
 
     function customizeConfigOff(name) {
         document.cookie = name + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
         document.documentElement.removeAttribute(name);
-    };
+    }
 
     //========================================================
     // Slide bar touch fix
@@ -165,7 +182,7 @@
         const searchInput = document.getElementById("search_text");
         const checkbox1 = document.getElementById("navMenuNeueToggle");
         document.querySelector("label[for='navMenuNeueToggle']").addEventListener("click", () => {
-            if (document.documentElement.getAttribute("enable_popup_keyboard") === "1" && !checkbox1.checked) {
+            if (document.documentElement.getAttribute("enable_popup_keyboard") === "on" && !checkbox1.checked) {
                 setTimeout(() => {
                     searchInput.focus();
                 }, 300);
@@ -202,7 +219,7 @@
             const customizePanel = document.getElementById("customize-panel");
             const isHidden = (window.getComputedStyle(customizePanel).display === "none");
             //console.log("isHidden: ", isHidden);
-            if (customizePanel && !isHidden && document.documentElement.getAttribute("enable_toggle_cp") === "1") {
+            if (customizePanel && !isHidden && document.documentElement.getAttribute("enable_toggle_cp") === "on") {
                 customizePanel.style.display = "none";
             } else {
                 _originalShowCP();
